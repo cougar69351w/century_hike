bd8dea91a04e6253be7157e734a9700af8d02523
Josh <moore.jsh@gmail.com>
2018-02-27 22:09:05 -0800

    ami update

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "century-hike-prod-web" {
-  ami           = "ami-adf8f3cd"
+  ami           = "ami-60f5fe00"
   instance_type = "t2.micro"
   key_name = "century-hike-prod"
   subnet_id = "${aws_subnet.private-subnet-us-west-1a.id}"
